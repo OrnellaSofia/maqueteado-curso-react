b8a4ed88f6d39f308da21a570b0c003b32c9e280
Ornella Acerbi <orneacerbi@gmail.com>
2020-11-13 10:49:03 -0300

    Repo iniciado

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.png';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <img src={logo} className="logo" alt="logo" />
+        <div className="div-search-bar">
+          <input type="text" className="search-bar" placeholder="Buscar productos, marcas y más..."/>
+        </div>
       </header>
+
+      <div className="content">
+        <div className="categories">
+          <h3>Notebook</h3>
+          <p>30.906 resultados</p>
+          <h4>Categorías</h4>
+          <ul>
+            <li>Categoría 1</li>
+            <li>Categoría 2</li>
+            <li>Categoría 3</li>
+            <li>Categoría 4</li>
+            <li>Categoría 5</li>
+          </ul>
+        </div>
+        <div className="products">
+          <div className="one-product">
+            Notebook
+            <img src="https://picsum.photos/200" />
+          </div>
+          <div className="one-product">
+            <img src="https://picsum.photos/200" />
+          </div>
+          <div className="one-product">
+            <img src="https://picsum.photos/200" />
+          </div>
+          <div className="one-product">
+              <img src="https://picsum.photos/200" />
+          </div>
+          <div className="one-product">
+              <img src="https://picsum.photos/200" />
+          </div>
+          <div className="one-product">
+              <img src="https://picsum.photos/200" />
+          </div>
+        </div>
+
+      </div>
     </div>
   );
 }
